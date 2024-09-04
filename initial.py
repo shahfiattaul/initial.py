6c2d0074b358876cf51049e27dba410862113ef9
Atta-ul-Shahfi <102184902+shahfiattaul@users.noreply.github.com>
2024-09-04 16:26:50 -0400

    nitial.py

---
+++ initial.py
@@ -0,0 +1,10 @@
+# Atta-ul Shahfi
+# I am learning to Code! This ia initial.py Project!
+
+print(" SSS    A  ")
+print("S   S  A A ")
+print("S     A   A")
+print(" SSS  AAAAA")
+print("    S A   A")
+print("S   S A   A")
+print(" SSS  A   A")
